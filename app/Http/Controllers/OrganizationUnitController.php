eafda3bac457a5c1c731305922a8e22a6e9a3e96
ahmadr127 <ahmdrifai808@gmail.com>
2026-04-30 12:00:22 +0700

    feat: implement OrganizationUnitController for CRUD operations and member management

--- app/Http/Controllers/OrganizationUnitController.php
+++ app/Http/Controllers/OrganizationUnitController.php
@@ -18,8 +18,8 @@ class OrganizationUnitController extends Controller
         if ($request->filled('search')) {
             $search = $request->search;
             $query->where(function($q) use ($search) {
-                $q->where('name', 'like', "%{$search}%")
-                  ->orWhere('code', 'like', "%{$search}%");
+                $q->where('name', 'ilike', "%{$search}%")
+                  ->orWhere('code', 'ilike', "%{$search}%");
             });
         }
 
